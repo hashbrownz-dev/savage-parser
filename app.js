01ad0f1b521cec4c1cfa4ded4710b14f74fe6cff
Roosevelt Blow <rooseveltblow@gmail.com>
2022-12-13 09:26:17 -0700

    adds getDims, getStyles, getLayerByName and getElement

--- app.js
+++ app.js
@@ -1,3 +1,101 @@
+// Get image dimensions from the viewBox attribute
+// expected output: viewBox="0 0 54 56"
+
+const getDims = (data) => {
+    const dims = data.match(/viewBox="(\d+) (\d+) (\d+) (\d+)"/);
+    return { drawW: dims[3], drawH: dims[4] }
+}
+
+// Get all data within the style tag.
+// Remove the style tag from the data.
+
+const getStyles = (data) => {
+    // Find the style element
+    const openingTag = `<style type="text/css">`,
+        closingTag = `</style>`;
+    const ind1 = data.indexOf(openingTag);
+    const ind2 = data.indexOf(closingTag) + closingTag.length;
+
+    // Split the style element into lines
+    const lines = data.substring(ind1, ind2).split('\n');
+
+    // Remove the first and last element of lines
+    lines.pop();
+    lines.shift();
+
+    // Process each line
+    const styles = [];
+    lines.forEach( line => {
+        styles.push(parseStyle(line));
+    })
+    return styles;
+}
+
+const parseStyle = (line) => {
+    // Get the className
+    const className = line.match(/\.(st\d+)/)[1];
+    // Get the string data between the { }
+    const index1 = line.indexOf('{')+1, index2 = line.indexOf('}',index1);
+    const sub = line.substring(index1,index2);
+    // Get our properties
+    const props = sub.split(';');
+    // Parse our properties
+    const parsedProps = props.filter( element => element).map( parseProp )
+    // Construct our Style Object
+    return Object.fromEntries([["className",className]].concat(parsedProps));
+}
+
+const parseProp = (prop) => {
+    return prop.split(':');
+}
+
+// groups = 'temp' 'fill || mask' 'stroke || main' 'hitBoxes'
+// {/* <g id="temp"></g> */}
+
+const getLayerByName = (data, layerName) => {
+    // Find the grouped elements
+    const openingTag = `<g id="${layerName}">`,
+        closingTag = `</g>`;
+    const ind1 = data.indexOf(openingTag);
+    const ind2 = data.indexOf(closingTag, ind1) + closingTag.length;
+
+    // Split the grouped elements into individual elements
+    const lines = data.substring(ind1, ind2).split('\n');
+
+    // Remove the first and last element of lines
+    lines.pop();
+    lines.shift();
+    const string = lines.join(''); // a solid string now... so now we need to go through that bitch and split our elements into an array...
+
+    // To do this...
+    // We start at index 0
+    // We find our element, a string that begins with '<' and ends with '/>'
+    // We push this string into an array.
+    // We update our index to the end of the last element.
+    // We repeat until we reach the end of the string.
+
+    let index = 0;
+    const elements = [];
+
+    while(index < string.length - 1){
+        const e = getElement(string, index);
+        elements.push(e[0]);
+        index = e[1];
+    }
+
+    return elements;
+}
+
+const getElement = (data, startInd = 0) => {
+    // Find each element
+    // This assumes that we are either in an element... or at a point in our document where we are dealing exclusively with shapes
+    const openingTag = `<`,
+        closingTag = `/>`;
+    const ind1 = data.indexOf(openingTag, startInd);
+    const ind2 = data.indexOf(closingTag, ind1) + closingTag.length;
+    return [data.substring(ind1, ind2), ind2];
+}
+
 const getClass = (line) => {
     if(line.includes('class')){
         const rx = /class="(st\d+)"/;
@@ -129,24 +227,6 @@ const parsePath = (line) => {
     }
 }
 
-const parseStyle = (line) => {
-    // Get the className
-    const className = line.match(/\.(st\d+)/)[1];
-    // Get the string data between the { }
-    const index1 = line.indexOf('{')+1, index2 = line.indexOf('}',index1);
-    const sub = line.substring(index1,index2);
-    // Get our properties
-    const props = sub.split(';');
-    // Parse our properties
-    const parsedProps = props.filter( element => element).map( parseProp )
-    // Construct our Style Object
-    return Object.fromEntries([["className",className]].concat(parsedProps));
-}
-
-const parseProp = (prop) => {
-    return prop.split(':');
-}
-
 const parseSVG = (data, fileName = '') => {
     const lines = data.split('\n');
     const styles = [];
@@ -194,6 +274,29 @@ const parseSVG = (data, fileName = '') => {
     
     return JSON.stringify(output);
 }
+
+// TEST IN BROWSER
+
+// SET INPUT AND OUTPUT ELEMENTS
+
+const createLog = (logName) => {
+    const div = document.createElement('div'),
+        heading = document.createElement('h1'),
+        paragraph = document.createElement('p');
+    heading.innerText = logName;
+    heading.style.textTransform = 'capitalize';
+    heading.style.fontSize = '1.5em';
+    div.appendChild(heading);
+    div.appendChild(paragraph);
+    document.body.appendChild(div);
+    // createLog returns a reference to the paragraph where our data will be displayed
+    return paragraph;
+}
+
+const inputLog = createLog('input:');
+const outputLog = createLog('output:');
+
+inputLog.innerText = testStr;
 
 // NODE
 
